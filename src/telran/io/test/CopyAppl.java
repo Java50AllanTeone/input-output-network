665bcb093bb1606a218776c3691f5c9b08b1cfa3
allanteone <allanteone@gmail.com>
2023-09-10 22:28:02 +0300

    hw-31

--- src/telran/io/test/CopyAppl.java
+++ src/telran/io/test/CopyAppl.java
@@ -1,10 +1,17 @@
 package telran.io.test;
 
+import java.nio.file.Files;
+
 public class CopyAppl {
 	public static void main(String[] args) {
 		//args[0] - source file
 //		args[2] - destination file
 		//args[3] - "overwrite"
+		
+		
+//		if (args.length < 2)
+//			System.out.println("Too few arguments");
+		System.out.println(Files.exists("hello.txt", LinkOptions.));
 		
 		
 	}
