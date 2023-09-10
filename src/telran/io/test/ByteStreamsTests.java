8432d43796411ab7f5b443d1df261c616cdaaeb9
allanteone <allanteone@gmail.com>
2023-09-10 22:23:51 +0300

    cw-31

---
+++ src/telran/io/test/ByteStreamsTests.java
@@ -0,0 +1,45 @@
+package telran.io.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ByteStreamsTests {
+	
+	private static final String DATA = "Hello world!!!";
+	private static final String FILE_NAME = "hello.txt";
+
+	@Test
+	@Order(1)
+	void fileOutputStream() throws Exception {
+
+		try (OutputStream output = new FileOutputStream(FILE_NAME)) {
+			output.write(DATA.getBytes());
+		}
+	}
+	
+	@Test
+	@Order(2)
+	void fileInputStream() throws Exception {
+		try(InputStream input = new FileInputStream(FILE_NAME)) {
+			int length = input.available();
+			byte[] buffer = new byte[length];
+			input.read(buffer);
+			assertEquals(DATA, new String(buffer));
+			
+		}
+	}
+
+	
+
+}
---
+++ src/telran/io/test/CopyAppl.java
@@ -0,0 +1,12 @@
+package telran.io.test;
+
+public class CopyAppl {
+	public static void main(String[] args) {
+		//args[0] - source file
+//		args[2] - destination file
+		//args[3] - "overwrite"
+		
+		
+	}
+
+}
