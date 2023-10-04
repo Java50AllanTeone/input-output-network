18820c93d42207637d81dafbe898512d1e6d2c19
allanteone <allanteone@gmail.com>
2023-10-04 21:19:26 +0300

    refactoring

--- src/MaxRatedWinnersCollector.java
+++ src/MaxRatedWinnersCollector.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.ToLongFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collector.Characteristics;
