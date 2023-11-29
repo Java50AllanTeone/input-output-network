be42b03e5b0ae439fc2b35fb5c5b118ba42cbc42
allanteone <allanteone@gmail.com>
2023-11-29 23:06:11 +0200

    hw-39

--- src/telran/net/CalculatorProtocol.java
+++ src/telran/net/CalculatorProtocol.java
@@ -1,37 +1,31 @@
 package telran.net;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiFunction;
 
 public class CalculatorProtocol implements ApplProtocol {
-    private static Map<String, BiFunction<String, String, String>> ops = Map.of(
-            "+", (o1, o2) -> Double.parseDouble(o1) + Double.parseDouble(o2) + "",
-            "-", (o1, o2) -> Double.parseDouble(o1) - Double.parseDouble(o2) + "",
-            "*", (o1, o2) -> Double.parseDouble(o1) * Double.parseDouble(o2) + "",
-            "/", (o1, o2) -> Double.parseDouble(o1) / Double.parseDouble(o2) + "",
-            "between", (o1, o2) -> ChronoUnit.DAYS.between(LocalDate.parse(o1), LocalDate.parse(o2)) + "",
-            "plusDate", (o1, o2) -> LocalDate.parse(o1).plusDays(Integer.parseInt(o2)) + "",
-            "minusDate", (o1, o2) -> LocalDate.parse(o1).minusDays(Integer.parseInt(o2)) + ""
+    private static Map<String, BiFunction<String, String, Serializable>> ops = Map.of(
+            "+", (o1, o2) -> Double.parseDouble(o1) + Double.parseDouble(o2),
+            "-", (o1, o2) -> Double.parseDouble(o1) - Double.parseDouble(o2),
+            "*", (o1, o2) -> Double.parseDouble(o1) * Double.parseDouble(o2),
+            "/", (o1, o2) -> Double.parseDouble(o1) / Double.parseDouble(o2),
+            "between", (o1, o2) -> ChronoUnit.DAYS.between(LocalDate.parse(o1), LocalDate.parse(o2)),
+            "plusDate", (o1, o2) -> LocalDate.parse(o1).plusDays(Integer.parseInt(o2)),
+            "minusDate", (o1, o2) -> LocalDate.parse(o1).minusDays(Integer.parseInt(o2))
     );
 
 
     @Override
     public Response getResponse(Request request) {
-        Response response;
-        String[] operands = ((String)request.requestData()).split("#");
+        String[] operands = ((String) request.requestData()).split("#");
+        var fn = ops.get(request.requestType());
 
-        if (operands.length != 2) {
-            response = new Response(ResponseCode.WRONG_DATA, "Wrong operands count");
-        } else {
-            response = handleRequest(request.requestType(), operands);
-        }
-        return response;
-    }
-
-    private Response handleRequest(String type, String[] tokens) {
-        BiFunction fn = ops.get(type);
-        return new Response(fn == null ? ResponseCode.WRONG_TYPE: ResponseCode.OK, (String)fn.apply(tokens[0], tokens[1]));
+        return operands.length != 2 ? new Response(ResponseCode.WRONG_DATA, "Wrong operands count") :
+                Objects.isNull(fn) ? new Response(ResponseCode.WRONG_TYPE, "Wrong operation") :
+                        new Response(ResponseCode.OK, fn.apply(operands[0], operands[1]));
     }
 }
