298058a5bd99e526f2fa9e5c0867ee8ee3ffc516
allanteone <allanteone@gmail.com>
2023-11-29 16:19:09 +0200

    cw-39

---
+++ src/telran/net/Request.java
@@ -0,0 +1,6 @@
+package telran.net;
+
+import java.io.Serializable;
+
+public record Request(String requestType, Serializable requestData) implements Serializable {
+}
---
+++ src/telran/net/Response.java
@@ -0,0 +1,6 @@
+package telran.net;
+
+import java.io.Serializable;
+
+public record Response(ResponseCode code, Serializable responseData) implements Serializable {
+}
